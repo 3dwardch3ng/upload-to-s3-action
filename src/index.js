73b9acdc93694db362747a26ca87dac11f311edf
Edward Cheng <edward.cheng@medirecords.com>
2022-05-18 13:12:57 +1000

    Fix: fix the check input param is set or not

--- src/index.js
+++ src/index.js
@@ -57,9 +57,7 @@ let s3Options = {apiVersion: '2006-03-01'};
 if (AWS_ACCESS_KEY_ID !== '' && AWS_SECRET_ACCESS_KEY !== '') {
   s3Options['accessKeyId'] = AWS_ACCESS_KEY_ID;
   s3Options['secretAccessKey'] = AWS_SECRET_ACCESS_KEY;
-}
-
-if (AWS_ASSUME_ROLE_ARN !== '') {
+} else if (AWS_ASSUME_ROLE_ARN !== '') {
   let stsOptions = {
     apiVersion: '2011-06-15',
     RoleArn: AWS_ASSUME_ROLE_ARN,
@@ -75,6 +73,8 @@ if (AWS_ASSUME_ROLE_ARN !== '') {
       s3Options['secretAccessKey'] = data.Credentials.SecretAccessKey;
     }
   });
+} else {
+  throw new Error('You need to either pass in both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY or AWS_ASSUME_ROLE_ARN');
 }
 
 const s3 = new aws.S3(s3Options);
